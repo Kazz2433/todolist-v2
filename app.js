bd49274d1c3a084144679e034d33654e491b7d09
armit4ge <kelvinquida24@gmail.com>
2022-05-15 23:31:33 -0400

    changes to package files 6

--- app.js
+++ app.js
@@ -146,9 +146,9 @@ app.get("/about", function(req, res){
 });
 
 let port = process.env.PORT
-// if(port == null || port == ""){
-//   port = 3000
-// }
+if(port == null || port == ""){
+  port = 3000
+}
 
 app.listen(port || 3000, function() {
   console.log("Server has started Successfully on dynamic port");
